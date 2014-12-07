90d8f4c770015caa30a9b3a5d69feab5b52e7376
David Parrish <daveparrish@gmail.com>
2014-12-07 06:48:47 -0500

    Complete working calculator with my hard coded commission.

--- btcprice.js
+++ btcprice.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     url: "https://api.bitcoinaverage.com/ticker/all",
 
     // Which of bitcoinaverages value we use to present prices
-    marketRateVariable: "24h_avg",
+    marketRateVariable: "last",
 
     // Which currencies are in shown to the user
     currencies: ["USD", "BTC"],
@@ -45,8 +45,104 @@ $(document).ready(function() {
     // checkout, put USD here.
     priceOrignalCurrency: "BTC"
   });
-});
 
-$(document).bind("marketdataavailable", function() {
-  console.log('Hey, marketdata might have changed');
+  // Reload price on clicking "Refresh"
+  $( "#refresh" ).click(function() {
+    $('#refresh').text('Refreshing')
+    $('#time').text('Refreshing time')
+    bitcoinprices.loadData();
+  });
+
+  // What happens when the price is updated?
+  $(document).bind("marketdataavailable", function() {
+    myTime = new Date(bitcoinprices.data.timestamp)
+    $('#time').text(myTime)
+    $('#refresh').text('Refresh')
+    calculate()
+  });
+
+  // Trigger calculation on text change.
+  $( "input" ).change(function() {
+    calculate(this.name)
+  });
+  $( "input" ).on('input', function() {
+    calculate(this.name)
+  });
+
+  // Rounding value to the nearest x
+  function nearest(n, v) {
+    n = n / v;
+    n = Math.round(n) * v;
+    return n;
+  }
+  // Rounding value to the x decimal
+  function precision(n, dp) {
+    dp = Math.pow(10, dp);
+    n = n * dp;
+    n = Math.round(n);
+    n = n / dp;
+    return n;
+  }
+
+  function init() {
+    // Set the commission
+    $("input[name='commission']").val('15')
+    $("input[name='commission']").prop('disabled', true);
+
+    // Set the BTC to 1 to give a starting calculation
+    $("input[name='buy_amount_BTC']").val('1')
+  }
+
+  // Calculate other fields based on the "change" parameter.
+  // "change" is the name of the field which has changed.
+  function calculate(change) {
+    buy_amount_USD = $("input[name='buy_amount_USD']").val()
+    buy_amount_BTC = $("input[name='buy_amount_BTC']").val()
+    buy_amount_mBTC = $("input[name='buy_amount_mBTC']").val()
+    total_USD = $("input[name='total_USD']").val()
+    commission = $("input[name='commission']").val()
+    rate = bitcoinprices.data.USD.last
+
+    // If the user knows how much BTC they want.
+    if (change == 'buy_amount_BTC') {
+      // buy_amount_BTC * rate
+      buy_amount_USD = precision(buy_amount_BTC * rate, 2)
+    }
+    if (change == 'buy_amount_mBTC') {
+      // (buy_amount_mBTC / 1000) * rate
+      buy_amount_USD = precision((buy_amount_mBTC / 1000) * rate, 2)
+    }
+
+    if (change != 'total_USD') {
+      // buy_amount_USD * (1 + commission / 100)
+      total_USD = nearest(buy_amount_USD * (1 + (commission / 100)), 5)
+      total_USD = (total_USD <= buy_amount_USD && buy_amount_USD != 0) ? total_USD + 5 : total_USD
+      $("input[name='total_USD']").val(total_USD)
+    }
+    if (change != 'buy_amount_USD') {
+      // total_USD / (1 + commission / 100)
+      buy_amount_USD = nearest(total_USD / (1 + (commission / 100)), 5)
+      $("input[name='buy_amount_USD']").val(buy_amount_USD)
+    }
+    if (change != 'buy_amount_BTC' && change != 'buy_amount_mBTC') {
+      // rate / buy_amount_USD
+      buy_amount_BTC = precision(buy_amount_USD / rate, 8)
+      $("input[name='buy_amount_BTC']").val(buy_amount_BTC)
+    }
+    if (change != 'buy_amount_BTC' && change == 'buy_amount_mBTC') {
+      // buy_amount_mBTC / 1000
+      buy_amount_BTC = precision(buy_amount_mBTC / 1000, 8)
+      $("input[name='buy_amount_BTC']").val(buy_amount_BTC)
+    }
+    if (change != 'buy_amount_mBTC') {
+      // buy_amount_BTC * 1000
+      buy_amount_mBTC = precision(buy_amount_BTC * 1000, 8)
+      $("input[name='buy_amount_mBTC']").val(buy_amount_mBTC)
+    }
+
+    // total_USD - buy_amount_USD
+    $('#fee_total').text(total_USD - buy_amount_USD)
+  }
+
+  init()
 });
