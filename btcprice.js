13449df4f13c60c1aabd402cf4f992251aaffd62
Donovan <donovan6925@gmail.com>
2017-08-04 09:55:21 -0400

    updated syntax

--- btcprice.js
+++ btcprice.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
   var rate = 0;
 
   function getPrice() {
-    $.ajax({url: "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", success: function(result){
+    $.ajax({
+      url: "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", success: function(result) {
       if (result && result.last) {
         rate = result.last;
         var myTime = new Date(result.timestamp * 1000);
@@ -61,44 +62,44 @@ $(document).ready(function() {
   // Calculate other fields based on the "change" parameter.
   // "change" is the name of the field which has changed.
   function calculate(change) {
-    buy_amount_USD = Number($("input[name='buy_amount_USD']").val());
-    buy_amount_BTC = Number($("input[name='buy_amount_BTC']").val());
-    buy_amount_mBTC = Number($("input[name='buy_amount_mBTC']").val());
-    total_USD = Number($("input[name='total_USD']").val());
-    commission = Number($("input[name='commission']").val());
+    var buy_amount_USD = Number($("input[name='buy_amount_USD']").val());
+    var buy_amount_BTC = Number($("input[name='buy_amount_BTC']").val());
+    var buy_amount_mBTC = Number($("input[name='buy_amount_mBTC']").val());
+    var total_USD = Number($("input[name='total_USD']").val());
+    var commission = Number($("input[name='commission']").val());
 
     // If the user knows how much BTC they want.
-    if (change == 'buy_amount_BTC') {
+    if (change === 'buy_amount_BTC') {
       // buy_amount_BTC * rate
       buy_amount_USD = precision(buy_amount_BTC * rate, 2);
     }
-    if (change == 'buy_amount_mBTC') {
+    if (change === 'buy_amount_mBTC') {
       // (buy_amount_mBTC / 1000) * rate
       buy_amount_USD = precision((buy_amount_mBTC / 1000) * rate, 2);
     }
 
-    if (change != 'total_USD') {
+    if (change !== 'total_USD') {
       // buy_amount_USD * (1 + commission / 100)
       total_USD = nearest(buy_amount_USD / (1 - (commission / 100.00)), 1);
       total_USD = (total_USD <= buy_amount_USD && buy_amount_USD !== 0) ? total_USD + 5 : total_USD;
       $("input[name='total_USD']").val(total_USD);
     }
-    if (change != 'buy_amount_USD') {
+    if (change !== 'buy_amount_USD') {
       // total_USD / (1 + commission / 100)
       buy_amount_USD = nearest(total_USD * (1 - (commission / 100.00)), 1);
       $("input[name='buy_amount_USD']").val(buy_amount_USD);
     }
-    if (change != 'buy_amount_BTC' && change != 'buy_amount_mBTC') {
+    if (change !== 'buy_amount_BTC' && change !== 'buy_amount_mBTC') {
       // rate / buy_amount_USD
       buy_amount_BTC = precision(buy_amount_USD / rate, 8);
       $("input[name='buy_amount_BTC']").val(buy_amount_BTC);
     }
-    if (change != 'buy_amount_BTC' && change == 'buy_amount_mBTC') {
+    if (change !== 'buy_amount_BTC' && change === 'buy_amount_mBTC') {
       // buy_amount_mBTC / 1000
       buy_amount_BTC = precision(buy_amount_mBTC / 1000, 8);
       $("input[name='buy_amount_BTC']").val(buy_amount_BTC);
     }
-    if (change != 'buy_amount_mBTC') {
+    if (change !== 'buy_amount_mBTC') {
       // buy_amount_BTC * 1000
       buy_amount_mBTC = precision(buy_amount_BTC * 1000, 8);
       $("input[name='buy_amount_mBTC']").val(buy_amount_mBTC);
